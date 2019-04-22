4553cf4efa5abc3caa62c6d3c601f661ff97c3fb
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-21 23:11:09 -0500

    Create basic dice logic

--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 import './Die.css'
 class Die extends Component {
+    static defaultProps = {
+        diceValues : [1,2,3,4,5,6]
+    }
     render() {
+        let translator = {
+            1: 'one',
+            2: 'two',
+            3: 'three',
+            4: "four",
+            5: 'five',
+            6: 'six'
+        }
+        let classNumber = `fas fa-dice-${translator[this.props.num]} fa-6x`
+        console.log(classNumber)
         return (
             <div className="Die">
-                <i className="fas fa-dice-one fa-6x"></i>
+                <i className={classNumber}></i>
             </div>
         )
     }
--- src/RollDice.js
+++ src/RollDice.js
@@ -3,14 +3,33 @@ import Die from './Die'
 import './RollDice.css'
 
 class RollDice extends Component {  
+    constructor(props) {
+        super(props)
+        this.state = {
+            dice1: 1, 
+            dice2: 1
+        }
+        this.roll = this.roll.bind(this)
+    }
+
+    roll() {
+        let num1, num2;
+        num1 = Math.floor(Math.random() * 5) + 1
+        num2 = Math.floor(Math.random() * 5) + 1
+        this.setState({
+            dice1: num1, 
+            dice2: num2}) 
+        }
+
+
     render() {
         return (
             <div className="RollDice">
             <div className="RollDice-Dice">
-                <Die />
-                <Die />
+                <Die num={this.state.dice1} />
+                <Die num={this.state.dice2}/>
             </div>
-                <button>Roll Dice</button>
+                <button onClick={this.roll}>Roll Dice</button>
                 
             </div>
         )
